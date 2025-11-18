4265c24f021a27000387c7cdc389b469c136fc1b
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-11-18 14:15:08 -0300

    adicionando display de tempo na tela

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -1,16 +1,16 @@
 $.ajax({
     url: "http://l5d1137.callbox.com.br/monitoramento/lib/ramais.php",
     type: "GET",
-    success: function(data){                
-        for(let i in data){
+    success: function (data) {
+        for (let i in data) {
             $('#cartoes').append(`<div class="cartao">
                                 <div>${data[i].nome}</div>
                                 <span class="${data[i].status} icone-posicao"></span>
                               </div>`)
         }
-        
+
     },
-    error: function(){
+    error: function () {
         console.log("Errouu!")
     }
 });
